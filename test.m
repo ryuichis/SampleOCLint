293dccb9673cad73e817dea717575891560d681e
Ryuichi Sai <ryuichi@rice.edu>
2017-11-16 10:24:20 -0600

    Try some simple cases; and print out intermediate outputs

---
+++ test.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  SampleOCLint
+//
+//  Created by Luciano Almeida on 13/11/17.
+//  Copyright © 2017 AIS Tecnologia da Informação Ltda. All rights reserved.
+//
+
+
+int main(int argc, char * argv[]) {
+  return 0;
+}
